41df67b9d4bb300b43c5257991345d8973fa7ae5
Rúben Freitas <joseruben98@hotmail.com>
2022-12-06 10:04:27 +0000

    9

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -38,6 +38,7 @@ class ReservationController extends Controller
      */
     public function store(ReservationRequest $request)
     {
+        return $request;
         $validator = $request->validated();
 
         DB::beginTransaction();
